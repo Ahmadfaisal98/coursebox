fd2025461d16bf9b62c6c461e52e4173d678f7dc
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-05 23:12:57 +0700

    reformat storybook main and preview

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-  "stories": [
+  stories: [
     "../pages/**/*.stories.mdx",
     "../pages/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.mdx",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)"
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "staticDirs": ['../public'],
-  "webpackFinal": async (config) => {
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
       new TsconfigPathsPlugin({
@@ -17,17 +17,18 @@ module.exports = {
     ];
     return config;
   },
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
   },
   // https://github.com/storybookjs/storybook/issues/10231
-  "features": {
-    "emotionAlias": false,
+  features: {
+    emotionAlias: false,
   },
-}
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,45 +1,48 @@
-import * as NextImage from 'next/image';
-import { ThemeProvider, Global } from '@emotion/react';
+import * as NextImage from "next/image";
+import { ThemeProvider, Global } from "@emotion/react";
 
-import { Themes } from '../styles/themes'
-import { GlobalStyles } from "../styles/global"
+import { Themes } from "../styles/themes";
+import { GlobalStyles } from "../styles/global";
 
 const OriginalNextImage = NextImage.default;
 
-Object.defineProperty(NextImage, 'default', {
+Object.defineProperty(NextImage, "default", {
   configurable: true,
   value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
 
 const withThemeProvider = (Story, context) => {
-  const background = context.globals.backgrounds?.value || parameters.backgrounds.defaultColor;
-  const theme = Object.values(Themes).find(theme => theme.background === background);
+  const background =
+    context.globals.backgrounds?.value || parameters.backgrounds.defaultColor;
+  const theme = Object.values(Themes).find(
+    (theme) => theme.background === background
+  );
   return (
     <ThemeProvider theme={theme}>
-      <Story {...context}/>
+      <Story {...context} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
 const withGlobalStyles = (Story, context) => (
   <>
-    <Global styles={GlobalStyles}/>
-    <Story {...context}/>
+    <Global styles={GlobalStyles} />
+    <Story {...context} />
   </>
-)
+);
 
-export const decorators = [withThemeProvider, withGlobalStyles]
+export const decorators = [withThemeProvider, withGlobalStyles];
 
 export const parameters = {
   backgrounds: {
-    default: 'dark',
-    defaultColor: '#5e5c64',
+    default: "dark",
+    defaultColor: "#5e5c64",
     values: [
-      {name: 'dark', value: '#5e5c64'},
-      {name: 'light', value: '#e4ebf5'},
-    ]
+      { name: "dark", value: "#5e5c64" },
+      { name: "light", value: "#e4ebf5" },
+    ],
   },
-  actions: { argTypesRegex: '^on[A-Z].*' },
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,

3ebe4c1e66d9d0e566e54edb25d8a576ac90d11b
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-05 22:08:02 +0700

    add TsconfigPathsPlugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   "stories": [
     "../pages/**/*.stories.mdx",
@@ -5,6 +7,15 @@ module.exports = {
     "../components/**/*.stories.mdx",
     "../components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+  "webpackFinal": async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",

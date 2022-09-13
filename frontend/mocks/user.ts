1e0bb589e7d04e4daf75202c684ca33ea6535d96
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-09-13 20:50:38 +0700

    add userSlice

---
+++ frontend/mocks/user.ts
@@ -0,0 +1,14 @@
+export const mockUser = {
+  jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYzMDYyNjc1LCJleHAiOjE2NjU2NTQ2NzV9.6_Su5rXd9o8uMRkHC-gAC0aT0dkSD0KoVj2SfEd7Q4I",
+  user: {
+    id: 2,
+    username: "John Doe",
+    email: "john@doe.com",
+    provider: "local",
+    confirmed: true,
+    blocked: false,
+    createdAt: "2022-09-12T03:46:44.404Z",
+    updatedAt: "2022-09-12T03:46:44.404Z",
+    password: "tes123!",
+  },
+};
---
+++ frontend/services/userSlice.test.ts
@@ -0,0 +1,93 @@
+import { mockUser } from "@/mocks/user";
+
+import { reducer, actions, initialState, login } from "./userSlice";
+
+const updatedState = {
+  jwt: mockUser.jwt,
+  username: mockUser.user.username,
+  email: mockUser.user.email,
+};
+
+const loginData = {
+  identifier: mockUser.user.email,
+  password: mockUser.user.password,
+};
+
+const requestId = "someId";
+
+describe("User slice check", () => {
+  describe("Update state actions", () => {
+    it("should update the full state", () => {
+      expect(reducer(initialState, actions.update(updatedState))).toEqual(
+        updatedState
+      );
+    });
+
+    it("should update only the jwt", () => {
+      expect(
+        reducer(initialState, actions.update({ jwt: updatedState.jwt }))
+      ).toEqual({ ...initialState, jwt: updatedState.jwt });
+    });
+
+    it("should clear the state", () => {
+      const stateWithUpdatedValues = reducer(
+        initialState,
+        actions.update(updatedState)
+      );
+
+      expect(stateWithUpdatedValues).toEqual(updatedState);
+
+      expect(reducer(stateWithUpdatedValues, actions.clear())).toEqual(
+        initialState
+      );
+    });
+  });
+
+  describe("Login state flow", () => {
+    it("should set the request state to pending", () => {
+      expect(
+        reducer(
+          { ...initialState, error: { message: "Rejected" } },
+          login.pending(requestId, loginData)
+        )
+      ).toEqual({
+        ...initialState,
+        requestState: "pending",
+      });
+    });
+    it("should set the request state to fulfill and reset any previous errors", () => {
+      expect(
+        reducer(
+          { ...initialState, error: { message: "Rejected" } },
+          login.fulfilled(
+            {
+              jwt: updatedState.jwt,
+              user: {
+                username: updatedState.username,
+                email: updatedState.email,
+              },
+            },
+            requestId,
+            loginData
+          )
+        )
+      ).toEqual({
+        ...updatedState,
+        requestState: "fulfilled",
+      });
+    });
+    it("should set the request state to rejected", () => {
+      const payloadError = { error: { name: "500", message: "Server error" } };
+      expect(
+        reducer(
+          initialState,
+          login.rejected({} as Error, requestId, loginData, payloadError)
+        )
+      ).toEqual({
+        ...initialState,
+        error: payloadError.error,
+        requestState: "rejected",
+      });
+    });
+  });
+});

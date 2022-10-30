6d9a4753a13c474619d718131300e8d0ccf7857a
ahmad.faisal <ahmad.faisal@bvarta.com>
2022-10-30 14:16:19 +0700

    handle condition crypto

--- frontend/hooks/useId.ts
+++ frontend/hooks/useId.ts
@@ -1,8 +1,22 @@
 import { useRef } from "react";
-import checkCrypto from "crypto";
+import crypto from "crypto";
+
+function dec2hex(dec: any) {
+  return dec.toString(16).padStart(2, "0");
+}
+
+// generateId :: Integer -> String
+function generateId(len = 40) {
+  var arr = new Uint8Array((len || 40) / 2);
+  if (window.crypto) {
+    window.crypto.getRandomValues(arr);
+    return Array.from(arr, dec2hex).join("");
+  }
+  return crypto.randomBytes(len / 2).toString("hex");
+}
 
 export const useId = (): string => {
-  const id = checkCrypto.randomBytes(10).toString("hex");
+  const id = generateId(20);
   const { current } = useRef(id);
   return current;
 };
